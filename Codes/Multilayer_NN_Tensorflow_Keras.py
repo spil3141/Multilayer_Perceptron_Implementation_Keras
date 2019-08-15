107329c928290e349a43a59083388c0bfd0a5e16
Seung-Pil Coleman <spil3141@naver.com>
2019-08-15 23:12:17 +0900

    debug

--- Codes/Multilayer_NN_Tensorflow_Keras.py
+++ Codes/Multilayer_NN_Tensorflow_Keras.py
@@ -85,8 +85,23 @@ history = model.fit(X_train_norm, y_train_onehot,
                     verbose=1,
                     validation_split=0.1)
 
+"""Serializing the model 
+   save model and architecture to single file
+"""
+model.save("../Models/MLP_Kerasmodel.h5")
+print("Saved model to disk")
 
-#Evaluation 
+# load and evaluate a saved model
+from tensorflow.keras.models import load_model
+ 
+# load model
+loaded_model = load_model("../Models/MLP_Kerasmodel.h5")
+# summarize model.
+loaded_model.summary()
+
+
+
+#Evaluation real
 y_test_pred = model.predict_classes(X_test_norm, 
                                     verbose=0)
 
@@ -95,71 +110,31 @@ test_acc = correct_preds / y_test.shape[0]
 print('Test accuracy: %.2f%%' % (test_acc * 100))
 
 
+#Evaluation loaded model
+y_test_pred = loaded_model.predict_classes(X_test_norm, 
+                                    verbose=0)
+
+correct_preds = np.sum(y_test == y_test_pred, axis=0) 
+test_acc = correct_preds / y_test.shape[0]
+print('Test accuracy: %.2f%%' % (test_acc * 100))
+
 #predicting new sample
-
-import PIL
-import numpy as np 
-
-def image_2_numpyArray(argv):
-    """
-    This function returns the pixel values.
-    The imput is a png file location.
-    """
-    from PIL import Image, ImageFilter
-    import numpy as np
-    
-    im = Image.open(argv).convert('L')
-    width = float(im.size[0])
-    height = float(im.size[1])
-    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
-
-    if width > height:  # check which dimension is bigger
-        # Width is bigger. Width becomes 20 pixels.
-        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
-        if (nheight == 0):  # rare case but minimum is 1 pixel
-            nheight = 1
-            # resize and sharpen
-        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
-        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
-    else:
-        # Height is bigger. Heigth becomes 20 pixels.
-        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
-        if (nwidth == 0):  # rare case but minimum is 1 pixel
-            nwidth = 1
-            # resize and sharpen
-        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
-        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
-
-    # newImage.save("sample.png
-
-    tv = list(newImage.getdata())  # get pixel values
-
-    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
-    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
-    x=[tva]
-    newArr=[[0 for d in range(28)] for y in range(28)]
-    k = 0
-    for i in range(28):
-        for j in range(28):
-            newArr[i][j]=x[0][k]
-            k=k+1
-    return np.asarray(newArr)
+from Image_NumpyArray_Converter import image_2_numpyArray
+img = image_2_numpyArray("../Datasets/img.png")
+data = img
+img = img.reshape(-1)
+del img
 
 
 
-img = imageprepare("../Datasets/img.png")
-
-img = np.asarray(newArr)
-
-
-img = np.asarray(im)
-data = img 
 new_sample = np.asarray(data)
-new_sample = MMS.transform(new_sample)
+new_sample = MMS.transform(new_sample.reshape((1,-1)))
 
+
+
+
+import matplotlib.pyplot as plt 
+plt.imshow(new_sample.reshape((28,28)),cmap="gray")
+plt.show()
 #Generalization on a single Sample 
-print (np.argmax(model.predict(new_sample.reshape((1,-1)))))
-
-
+print (np.argmax(loaded_model.predict(new_sample.reshape((1,-1)))))
